f359d991e9c1e5fec07c4e51fdc17269c4336e38
Alvaro <amunoz@telecomboutique.com>
2020-12-18 18:34:58 +0100

    Añadido ESlint y configuración de AirBNB

--- webpack.config.js
+++ webpack.config.js
@@ -4,48 +4,57 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { HotModuleReplacementPlugin } = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: "bundle.[contenthash].js",
-        path: path.resolve(__dirname, "dist"),
-        publicPath: "/",
-    },
-    devtool: "eval-cheap-source-map",
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
-                resolve: {
-                    extensions: ['.js', '.jsx']
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-                exclude: /node_modules/
-            }
-        ]
-    },
-    devServer: {
-        historyApiFallback: true,
-        contentBase: path.resolve(__dirname, 'dist'),
-        open: true,
-        compress: true,
-        hot: true,
-        port: 8080,
-        watchOptions:{
-            poll: true,
-            ignored: "/node_modules/"
-        }
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  devtool: 'eval-cheap-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader', 'eslint-loader'],
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.js', '.jsx'],
+        },
       },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        }),
-        new HotModuleReplacementPlugin()
-    ]
-}
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|gif|jpg|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 50000,
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+    watchOptions: {
+      poll: true,
+      ignored: /node_modules/,
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new HotModuleReplacementPlugin(),
+  ],
+};
